82cec2f79640c902863e2d90e8e38c5c3d81d61f
iCarlosAugusto <augusto.dev@proton.me>
2024-07-08 10:52:24 -0300

    refactor: :recycle: tirando margin do botao

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -16,7 +16,7 @@ export default function ButtonComponent({
   return (
     <>
       <div
-        className={`mb-7.5 flex flex-wrap gap-5 xl:gap-20 ${className} ${disable ? 'opacity-50' : 'opacity-100'}`}
+        className={`flex flex-wrap gap-5 xl:gap-20 ${className} ${disable ? 'opacity-50' : 'opacity-100'}`}
         onClick={() => !disable ?? onClick}
       >
         <button className="inline-flex items-center justify-center rounded-md bg-meta-3 px-10 py-4 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
