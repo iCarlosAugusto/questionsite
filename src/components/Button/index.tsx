63276ad6cab9d95ca6ebaf3434693e74c4dc5261
iCarlosAugusto <augusto.dev@proton.me>
2024-07-31 20:28:19 -0300

    refactor: :recycle: melhorado btn

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -16,17 +16,12 @@ export default function ButtonComponent({
   onClick,
 }: ButtonProps) {
   return (
-    <>
-      <div
-        className={`flex flex-wrap gap-5 xl:gap-20 ${className} ${disable ? 'opacity-50' : 'opacity-100'}`}
-        onClick={() => (disable == true ? null : onClick?.())}
-      >
-        <button
-          className={`${className} inline-flex items-center justify-center rounded-md bg-meta-3 px-10 py-4 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10`}
-        >
-          {label}
-        </button>
-      </div>
-    </>
+    <button
+      className={`flex flex-wrap gap-5 xl:gap-20 ${className} ${disable ? 'opacity-50' : 'opacity-100'} ${className} inline-flex items-center justify-center rounded-md bg-meta-3 px-10 py-4 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10`}
+      disabled={disable}
+      onClick={() => (disable == true ? null : onClick?.())}
+    >
+      {label}
+    </button>
   );
 }
