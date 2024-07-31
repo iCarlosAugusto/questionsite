0a9343984f708e923d35800351b9b30613450500
iCarlosAugusto <augusto.dev@proton.me>
2024-07-30 22:44:24 -0300

    feat: :sparkles: melhorado componentes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -21,7 +21,9 @@ export default function ButtonComponent({
         className={`flex flex-wrap gap-5 xl:gap-20 ${className} ${disable ? 'opacity-50' : 'opacity-100'}`}
         onClick={() => (disable == true ? null : onClick?.())}
       >
-        <button className="inline-flex items-center justify-center rounded-md bg-meta-3 px-10 py-4 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
+        <button
+          className={`${className} inline-flex items-center justify-center rounded-md bg-meta-3 px-10 py-4 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10`}
+        >
           {label}
         </button>
       </div>
