2d3bcaa93342a335fe301df01120933d49d38dfd
iCarlosAugusto <augusto.dev@proton.me>
2024-08-04 23:15:08 -0300

    feat: :sparkles: modal animation back

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { easeIn, motion } from 'framer-motion';
 
@@ -16,6 +16,7 @@ const modalVariants = {
   hidden: {
     opacity: 0,
     y: '-100vh',
+    transition: { duration: 0.5 },
   },
   visible: {
     opacity: 1,
@@ -29,63 +30,73 @@ interface ModalProps {
 }
 
 const Modal = ({ isOpen, closeModal }: ModalProps) => {
-  if (!isOpen) return null;
+  const [currentAnimation, setCurrentAnimation] = useState<
+    'hidden' | 'visible'
+  >('visible');
+
+  const handleCloseAnimation = async () => {
+    setCurrentAnimation('hidden');
+    await new Promise((resolve) => setTimeout(resolve, 300));
+    closeModal();
+  };
 
   return (
-    <motion.div
-      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-999999 p-4"
-      variants={backdropVariants}
-      initial="hidden"
-      animate="visible"
-      exit="hidden"
-      onClick={closeModal}
-    >
+    isOpen && (
       <motion.div
-        className="rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8 mt-5 mb-5"
-        variants={modalVariants}
+        className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-999999 p-4"
+        variants={backdropVariants}
         initial="hidden"
         animate="visible"
         exit="hidden"
-        onClick={(e) => e.stopPropagation()}
+        onClick={handleCloseAnimation}
       >
-        <div className="flex items-center justify-between mb-4">
-          <span className="text-xl font-bold  text-orange-500">
-            Entrar na sua conta
-          </span>
-          <Image
-            src={Close}
-            alt="Close button"
-            width={25}
-            height={25}
-            className="cursor-pointer"
-            onClick={closeModal}
-          />
-        </div>
+        <motion.div
+          className="rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8 mt-5 mb-5"
+          variants={modalVariants}
+          initial="hidden"
+          animate={currentAnimation}
+          exit="hidden"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="flex items-center justify-between mb-4">
+            <span className="text-xl font-bold  text-orange-500">
+              Entrar na sua conta
+            </span>
+            <Image
+              src={Close}
+              alt="Close button"
+              width={25}
+              height={25}
+              className="cursor-pointer"
+              onClick={handleCloseAnimation}
+            />
+          </div>
 
-        <p className="mb-4">
-          Seja bem-vindo! Ficamos felizes em ter você por aqui!
-          <br />
-          Preparado para tirar sua certificação?
-        </p>
-        <Link href="/auth/signin">
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded w-full"
-            onClick={closeModal}
-          >
-            Acessar conta
-          </button>
-        </Link>
+          <p className="mb-4">
+            Seja bem-vindo! Ficamos felizes em ter você por aqui!
+            <br />
+            Preparado para tirar sua certificação?
+          </p>
+          <Link href="/auth/signin">
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded w-full"
+              onClick={closeModal}
+            >
+              Acessar conta
+            </button>
+          </Link>
 
-        <Link href="/auth/signup">
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded w-full mt-5"
-            onClick={closeModal}
-          >
-            Criar conta
-          </button>
-        </Link>
+          <Link href="/auth/signup">
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded w-full mt-5"
+              onClick={closeModal}
+            >
+              Criar conta
+            </button>
+          </Link>
+        </motion.div>
       </motion.div>
-    </motion.div>
+    )
   );
 };
 
--- src/components/Question/QuestionWrapper.tsx
+++ src/components/Question/QuestionWrapper.tsx
@@ -38,7 +38,7 @@ export function QuestionWrapper({
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8 mt-5 mb-5">
       <span>Q-{id} | AWS</span>
-      <Modal isOpen={isOpen} closeModal={closeModal} />
+      {isOpen && <Modal isOpen={isOpen} closeModal={closeModal} />}
       {children}
       {alternatives.map((currentAlternative, index) => (
         <div onClick={() => pickAlternative(currentAlternative)} key={index}>
