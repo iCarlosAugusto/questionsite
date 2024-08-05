8271a2d7f04416be14272a24ab087e3f6730353a
iCarlosAugusto <augusto.dev@proton.me>
2024-08-04 22:40:36 -0300

    feat: :sparkles: bordar red no input

--- src/components/Textfield/index.tsx
+++ src/components/Textfield/index.tsx
@@ -30,7 +30,7 @@ export function Textfield({
             name={name}
             type={type}
             placeholder={placeholder}
-            className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+            className={`w-full rounded-lg border ${textError ? '!border-red' : 'border-stroke'}  bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary `}
           />
           {textError && <span className="text-red font-bold">{textError}</span>}
 
